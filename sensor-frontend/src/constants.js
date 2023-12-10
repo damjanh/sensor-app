bb288aac72ecf2b0146da769cc7c0a8b6a9b1731
damjanh <damjan.hudobreznik@gmail.com>
2023-12-10 20:30:38 +0100

    Add input constants.

---
+++ sensor-frontend/src/constants.js
@@ -0,0 +1,14 @@
+const MEASUREMENT_QUERY_TYPES = [
+    'pm10',
+    'pm25'
+];
+
+const MEASUREMENT_RANGES = [
+    6, 12, 24
+];
+
+const MEASUREMENT_TYPES = [
+    "temperature", "humidity", "pressure", "pm10", "pm25"
+]
+
+export {MEASUREMENT_TYPES, MEASUREMENT_QUERY_TYPES, MEASUREMENT_RANGES};
