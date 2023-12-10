774be2ec4c862fe3b2280aca9b16f90dd391f7d2
damjanh <damjan.hudobreznik@gmail.com>
2023-12-10 21:35:24 +0100

    Prefill db for testing.

---
+++ sensor-backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into sensor (comments,description,position,status,type,id) values ('v1 sensor at Drachevo','Drachevo','41.934689175939155,21.527361273765564','ACTIVE','2',1);
+insert into sensor (comments,description,position,status,type,id) values ('pulse.eco WiFi v2 sensor located at Petrovec','Drachevo - Kuzman','41.94143946567824,21.520919411765377','ACTIVE','3',2);
+insert into sensor (comments,description,position,status,type,id) values ('Imported Sensor.community #78662','Sensor.community 78662','42.02360504614,21.35250452673','NOT_CLAIMED','20004',3);
