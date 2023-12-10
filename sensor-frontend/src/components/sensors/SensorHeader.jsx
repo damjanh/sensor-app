8533344f30a9c62269c5c328665f4bd4e9fdd5ad
damjanh <damjan.hudobreznik@gmail.com>
2023-12-10 21:35:44 +0100

    Style change for Sensor items.

--- sensor-frontend/src/components/sensors/SensorHeader.jsx
+++ sensor-frontend/src/components/sensors/SensorHeader.jsx
@@ -2,16 +2,16 @@ const SensorHeader = () => {
     return (
         <div className={"list-group-item list-group-item-secondary"} >
         <div className="row nowrap">
-            <div className='col'>
+            <div className='col-1'>
                 Type
             </div>
-            <div className='col'>
+            <div className='col-4'>
                 Description
             </div>
-            <div className='col-6'>
+            <div className='col-5'>
                 Comments
             </div>
-            <div className='col'>
+            <div className='col-2'>
                 Status
             </div>
         </div>
--- sensor-frontend/src/components/sensors/SensorItem.jsx
+++ sensor-frontend/src/components/sensors/SensorItem.jsx
@@ -2,16 +2,16 @@ const SensorItem = ({ sensor, handleSensorSelect, active }) => {
     return (
         <li className={active ? "list-group-item list-group-item-action active" : "list-group-item list-group-item-action"} onClick={() => { handleSensorSelect(sensor.id) }}>
             <div className="row nowrap">
-                <div className='col'>
+                <div className='col-1'>
                     <small>{sensor.type}</small>
                 </div>
-                <div className='col'>
+                <div className='col-4 text-truncate' >
                     <small>{sensor.description}</small>
                 </div>
-                <div className='col-6'>
+                <div className='col-5 text-truncate'>
                     <small>{sensor.comments}</small>
                 </div>
-                <div className='col'>
+                <div className='col-2 text-truncate'>
                     <small>{sensor.status}</small>
                 </div>
             </div>
